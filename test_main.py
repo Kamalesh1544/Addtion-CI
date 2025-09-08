70d3e55d7552a9ed328ed68f79a7e45086348500
Kamalesh1544 <kamaleshjohnson7@gmail.com>
2025-09-07 22:35:16 -0700

    Demo Addtion CI

---
+++ test_main.py
@@ -0,0 +1,33 @@
+import pytest
+from main import add
+
+@pytest.mark.parametrize("a,b.expected",[
+    (1,1,2),
+    (1,2,3),
+    (2,2,4)
+])
+def test_add(a,b,expected):
+    assert a+b == expected
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
